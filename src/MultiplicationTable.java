fa034c535ce03522a24e83c9fa3dd074bda7162d
nina222222 <nangryk@gmail.com>
2026-09-05 14:21:04 -0400

    submit

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,10 @@
+public class MultiplicationTable {
+    public static void main(String[] args){
+        or (int i = 0; i <= 12; i++){
+        for (int j = 0; j <= 12; j++){
+          System.out.print(i + j +" ");
+        }
+        System.out.println();
+      }
+    }
+}
