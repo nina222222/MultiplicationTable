11a64bd03a70b87633aad05d8d8d7dc144f01fc5
nina222222 <nangryk@gmail.com>
2026-09-06 15:46:49 -0400

    Fix multiplication logic in MultiplicationTable

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -1,8 +1,8 @@
 public class MultiplicationTable {
     public static void main(String[] args){
-        for (int i = 0; i <= 12; i++){
-        for (int j = 0; j <= 12; j++){
-          System.out.print(i + j +" ");
+        for (int i = 1; i <= 12; i++){
+        for (int j = 1; j <= 12; j++){
+          System.out.print(i * j +" ");
         }
         System.out.println();
       }
